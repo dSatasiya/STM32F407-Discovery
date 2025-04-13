4c28705de3332a5dcf2170f134b7acede93fc2cf
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2025-04-13 12:41:50 -0400

    Create main.h

---
+++ RCC/main.h
@@ -0,0 +1,20 @@
+/*
+ * @Author Dhruv Satasiya
+ * */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+
+#endif /* __MAIN_H */
