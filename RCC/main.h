e3e60ca26202af31d44787006f628e770b772084
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2025-04-13 13:37:12 -0400

    Update main.h

--- RCC/main.h
+++ RCC/main.h
@@ -13,6 +13,19 @@
 #include <string.h>
 #include <inttypes.h>
 
+/*
+ * Macros Definitions
+ * */
+#define TIM_BASE_INITS			0
+#define TIM_BASE_START			0
+#define UART_INITS				1
+#define UART_START				1
+
+/*System Clock Frequency Macors*/
+#define SYSCLK_FREQ_50MHz		50
+#define SYSCLK_FREQ_84MHz		84
+#define SYSCLK_FREQ_120MHz		120
+
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
