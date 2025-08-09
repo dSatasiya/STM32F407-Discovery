ff0b2baa985808dfe25d8b7b0a157d386fc327c8
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2025-08-09 18:21:09 -0400

    Create stm32f4xx_it.h

---
+++ Projects/Lower_to_Upper_Case_converter/stm32f4xx_it.h
@@ -0,0 +1,24 @@
+/* This header file is generated automatically during the
+ * STM32Cube project creation only necessary change is made
+ * */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32F4xx_IT_H
+#define __STM32F4xx_IT_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Private includes ----------------------------------------------------------*/
+
+/* Exported functions prototypes ---------------------------------------------*/
+void SysTick_Handler(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32F4xx_IT_H */
