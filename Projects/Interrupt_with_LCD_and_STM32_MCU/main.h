882bed77b3f6fe8e11728b7e9647d413024ee4f7
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2025-10-10 10:46:52 -0400

    Create main.h

---
+++ Projects/Interrupt_with_LCD_and_STM32_MCU/main.h
@@ -0,0 +1,42 @@
+/*
+ * Created on:	October 10, 2025
+ * 	   Author:	Dhruv Satasiya
+ * */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/*
+ * Some Basic Macros
+ * */
+#define TRUE						1
+#define FALSE						0
+#define ON							TRUE
+#define OFF							FALSE
+
+/*
+ * LCD Pin Macros
+ * */
+#define LCD_PIN_RS					GPIO_PIN_0
+#define LCD_PIN_E					GPIO_PIN_1
+
+#define LCD_PIN_D4					GPIO_PIN_4
+#define LCD_PIN_D5					GPIO_PIN_5
+#define LCD_PIN_D6					GPIO_PIN_6
+#define LCD_PIN_D7					GPIO_PIN_7
+
+#define INTERRUPT_LED				GPIO_PIN_13
+
+
+#endif /* __MAIN_H */
